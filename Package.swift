9e8a3a8c9a32b69177551b30953a46641fd7ca2d
hyunwookwon <khwkfc@wafour.com>
2025-10-01 17:39:08 +0900

    no message

--- Package.swift
+++ Package.swift
@@ -1,8 +1,30 @@
-//
-//  Package.swift
-//  aipang
-//
-//  Created by hyunwoo kwon on 10/1/25.
-//
+// swift-tools-version:5.7
+import PackageDescription
 
-import Foundation
+let package = Package(
+    name: "KTVHTTPCache",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "KTVHTTPCache",
+            targets: ["KTVHTTPCache"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "KTVHTTPCache",
+            path: "KTVHTTPCache/Classes",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("KTVHCCommon"),
+                .headerSearchPath("KTVHCDataStorage"),
+                .headerSearchPath("KTVHCDownload"),
+                .headerSearchPath("KTVHCHTTPServer"),
+                .headerSearchPath("KTVHCTools"),
+            ]
+        )
+    ]
+)
